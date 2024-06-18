b43bdba27ec0888f3f01a7de19b3bebd73cb9f5d
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-06-17 23:57:08 -0400

    Consolidating the functions

--- Role_Playing_Game/script.js
+++ Role_Playing_Game/script.js
@@ -59,17 +59,17 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location){
-
-}
-
-function goTown() {
-    button1.innerText = "Go to store";
+  button1.innerText = "Go to store";
     button2.innerText = "Go to cave";
     button3.innerText = "Fight dragon";
     button1.onclick = goStore;
     button2.onclick = goCave;
     button3.onclick = fightDragon;
     text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
+
+function goTown() {
+
 }
 /*
 The innerText property controls the text that appears in an HTML element. For example:
