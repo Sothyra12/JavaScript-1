1f267e99f82e61efce2a1defd92b7c10a0b589ac
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-06-25 23:57:07 -0400

    using Math Object in JavaScript

--- Role_Playing_Game/script.js
+++ Role_Playing_Game/script.js
@@ -317,3 +317,11 @@ Example Code
 const paragraph = document.querySelector('p');
 paragraph.style.display = 'block';
 */
+
+/*
+The Math object in JavaScript contains static properties and methods for mathematical constants and functions. One of those is Math.random(), which generates a random number from 0 (inclusive) to 1 (exclusive). Another is Math.floor(), which rounds a given number down to the nearest integer.
+
+Using these, you can generate a random number within a range. For example, this generates a random number between 1 and 5: Math.floor(Math.random() * 5) + 1;.
+
+Following this pattern, use the addition operator (+) to add a random number between 1 and the value of xp to your monsterHealth -= weapons[currentWeaponIndex].power.
+*/
