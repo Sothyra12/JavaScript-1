1e625bbb68970603fe7271b4e98a22303ba69cec
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-07-14 23:57:47 -0400

    added a label element

--- Calorie_Counter/script.js
+++ Calorie_Counter/script.js
@@ -164,10 +164,13 @@ function addEntry() {
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
 }
 
+*Now you need to build your dynamic HTML string to add to the webpage.
+Declare a new HTMLString variable, and assign it an empty template literal string.
+
 */
 
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-  let HTMLString = ``;
+  let HTMLString = `<label for="entry${entryNumber}">Entry ${entryNumber} Name:</label>`;
 }
