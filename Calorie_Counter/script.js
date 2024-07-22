2206befa8cfe3a64a8eb447b71e0dfc93c62edc3
Sothyra12 <schan256@my.centennialcollege.ca>
2024-07-21 23:29:50 -0400

    added 1 to the .length

--- Calorie_Counter/script.js
+++ Calorie_Counter/script.js
@@ -193,19 +193,34 @@ const formContent = `
   <input id="last-name" type="text">
 `;
 formElement.innerHTML += formContent;
+
+
+****Try adding a couple of entries to the Breakfast category,
+and you may notice some bugs! The first thing we need to fix is the entry counts –
+the first entry should have a count of 1, not 0.
+This bug occurs because you are querying for input[type="text"] elements
+before adding the new entry to the page.
+To fix this, update your entryNumber variable to be the value of the length of the query plus 1.
+Add this on your declaration line, not in your template strings.
+
 */
 
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-  let HTMLString = `
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"></input>
-  `;
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
   targetInputContainer.innerHTML += HTMLString;
 }
+
 
 
 /*
@@ -222,6 +237,7 @@ function printName() {
   console.log("Jessica");
 }
 button.addEventListener('click', printName);
+
 
 
 */
