7051c76b23e44b457f75b74b9ac06e9d786cfda0
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-08-10 23:25:41 -0400

    added the preventDefault() method to prevent the default action of the submit event

--- Calorie_Counter/script.js
+++ Calorie_Counter/script.js
@@ -306,7 +306,7 @@ to indicate that the function has failed.
 Still within your if block, set isError to true and return null.
 
 
-**
+
 */
 function getCaloriesFromInputs(list) {
   let calories = 0;
@@ -321,4 +321,22 @@ function getCaloriesFromInputs(list) {
     calories += Number(currVal);
   }
   return calories;
+}
+
+
+/*
+**Now it's time to start putting it all together. Declare an empty calculateCalories function, 
+which takes a parameter named e. This function will be another event listener, 
+so the first argument passed will be the browser event – e is a common name for this parameter.
+
+**You will be attaching this function to the submit event of the form. 
+The submit event is triggered when the form is submitted. 
+The default action of the submit event is to reload the page. 
+You need to prevent this default action using the preventDefault() method of your e parameter.
+Add a line to your calculateCalories function that calls the preventDefault() method on the e parameter. 
+Then, reset your global error flag to false.
+*/
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
 }
