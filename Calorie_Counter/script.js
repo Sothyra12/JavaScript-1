076390f3aeaec906de719db0431f12f66e2ad19d
Sothyra Chan <schan256@my.centennialcollege.ca>
2024-08-10 23:43:37 -0400

    added a new variable

--- Calorie_Counter/script.js
+++ Calorie_Counter/script.js
@@ -335,8 +335,14 @@ The default action of the submit event is to reload the page.
 You need to prevent this default action using the preventDefault() method of your e parameter.
 Add a line to your calculateCalories function that calls the preventDefault() method on the e parameter. 
 Then, reset your global error flag to false.
+
+**Your function needs to get the values from the entries the user has added.
+
+Declare a breakfastNumberInputs variable, and give it the value of calling document.querySelectorAll() 
+with the selector #breakfast input[type=number]. This will return any number inputs that are in the #breakfast element.
 */
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
+  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
 }
