3f5cec4d6111527ad1cf2e076de33d9a52d16aba
Sothyra12 <schan256@my.centennialcollege.ca>
2024-08-20 23:59:06 -0400

    added a new variable containing a nodelist array

--- Calorie_Counter/script.js
+++ Calorie_Counter/script.js
@@ -358,6 +358,18 @@ with breakfastNumberInputs as the argument.
 **Now declare a lunchCalories variable,
 and give it the value of calling getCaloriesFromInputs
 with your lunchNumberInputs.
+
+**You also need to get the value of your #budget input.
+You already queried this at the top of your code,
+and set it to the budgetNumberInput variable.
+However, you used getElementById, which returns an Element,
+not a NodeList. A NodeList is an array-like,
+which means you can iterate through it and it shares some
+common methods with an array. For your getCaloriesFromInputs function,
+an array will work for the argument just as well as a NodeList does.
+Declare a budgetCalories variable and set it to the result of
+calling getCaloriesFromInputs – pass an array containing your
+budgetNumberInput as the argument.
 */
 function calculateCalories(e) {
   e.preventDefault();
@@ -374,4 +386,5 @@ function calculateCalories(e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 }
