4c15a31e8c1f5732c5304230b5bc40bb13ccf611
Sothyra12 <schan256@my.centennialcollege.ca>
2024-08-24 22:04:10 -0400

    added a new variable with a ternary operator as its value

--- Calorie_Counter/script.js
+++ Calorie_Counter/script.js
@@ -370,6 +370,16 @@ an array will work for the argument just as well as a NodeList does.
 Declare a budgetCalories variable and set it to the result of
 calling getCaloriesFromInputs – pass an array containing your
 budgetNumberInput as the argument.
+
+**You need to know if the user is in a caloric surplus or deficit.
+A caloric surplus is when you consume more calories than you burn,
+and a caloric deficit is when you burn more calories than you consume.
+Burning as many calories as you consume is called maintenance,
+and can be thought of as a surplus or deficit of 0, depending on your goals.
+Declare a surplusOrDeficit variable.
+Then use a ternary operator to set surplusOrDeficit to the string "Surplus" or "Deficit"
+depending on whether remainingCalories is less than 0. If it is less than 0,
+then surplusOrDeficit should be "Surplus". Otherwise, it should be "Deficit".
 */
 function calculateCalories(e) {
   e.preventDefault();
@@ -394,4 +404,5 @@ function calculateCalories(e) {
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
 }
