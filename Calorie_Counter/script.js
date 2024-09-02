ecc980b12e7d4490da33cf192e46d859dda52c85
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-01 22:42:28 -0400

    wrapped my inputContainers query selector in Array.from()

--- Calorie_Counter/script.js
+++ Calorie_Counter/script.js
@@ -440,6 +440,25 @@ Start by declaring an empty function called clearForm – it should not take any
 
 **You need to get all of the input containers. Declare an inputContainers variable, and
 assign it to the value of querying the document for all elements with the class input-container.
+
+**Remember that document.querySelectorAll returns a NodeList,
+which is array-like but is not an array. However, the Array object has a
+.from() method that accepts an array-like and returns an array.
+This is helpful when you want access to more robust array methods,
+which you will learn about in a future project.
+The following example takes a NodeList of li elements and converts it to an array of li elements:
+Example Code
+<ul>
+  <li>List 1</li>
+  <li>List 2</li>
+  <li>List 3</li>
+</ul>
+
+Example Code
+  const listItemsArray = Array.from(document.querySelectorAll('li'));
+  console.log(listItemsArray); //Output: (3) [li, li, li]
+Wrap your inputContainers query selector in Array.from().
+Do this on the same line as your declaration.
 */
 function calculateCalories(e) {
   e.preventDefault();
@@ -475,3 +494,7 @@ function calculateCalories(e) {
 }
 
 calorieCounter.addEventListener("submit", calculateCalories);
+
+function clearForm() {
+  const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+}
